00d17897070f463755494157d9b5b566cb2ea179
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-09-01 22:48:32 +0300

    chore: Remove unnecessary files and update Python version requirement

--- api/index.py
+++ api/index.py
@@ -1,8 +1,13 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import requests
 
 app = FastAPI()
-
+# TOKEN = "7148305891:AAFvN7nJ_QIW_OA6OaJYa-v1C0efTz6kL_0"
+# CHAT_ID = "-4514991347"
+TOKEN: str | None = os.getenv(key="TOKEN")
+CHAT_ID: str | None = os.getenv(key="CHAT_ID")
 
 @app.get(path="/")
 def home() -> HTMLResponse:
@@ -10,3 +15,14 @@ def home() -> HTMLResponse:
         content="<h1>Welcome to the Backend of <a href='https://MH0386.github.io'>MH0386.github.io</a></h1>",
         status_code=200,
     )
+
+@app.get(path="/send")
+def send_message(text: str):
+    response: requests.Response = requests.post(
+        url=f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text,
+        },
+    )
+    return response.json()
