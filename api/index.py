25f718736f93bcf485dac4ee823329c9a3f22395
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-09-03 15:32:31 +0300

    chore: Integrate mailersend for sending messages in API code and update requirements

--- api/index.py
+++ api/index.py
@@ -1,15 +1,9 @@
 import os
-
-import mailersend
 import requests
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-mailer = mailersend.NewApiClient()
 app = FastAPI()
-TOKEN: str = os.getenv(key="TOKEN")
-CHAT_ID: str = os.getenv(key="CHAT_ID")
-
 
 @app.get(path="/")
 def home() -> HTMLResponse:
@@ -22,19 +16,10 @@ def home() -> HTMLResponse:
 @app.get(path="/send")
 def send_message(text: str):
     response_telegram: requests.Response = requests.post(
-        url=f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        url=f"https://api.telegram.org/bot{os.getenv(key='TOKEN')}/sendMessage",
         data={
-            "chat_id": CHAT_ID,
+            "chat_id": os.getenv(key="CHAT_ID"),
             "text": text,
         },
     )
-
-    subject = "New request from MH0386.github.io"
-    text = "There is a new request from your website."
-    html = "There is a new request from your website."
-
-    my_mail = "info@domain.com"
-    subscriber_list = ["mohamed.hisham.abdelzaher@gmail.com"]
-
-    mailer.send(my_mail, subscriber_list, subject, html, text)
     return response_telegram.json()
