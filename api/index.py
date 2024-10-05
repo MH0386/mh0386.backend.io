7917243289864e5f5e063df66a5df896144bb071
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-10-05 16:55:03 +0300

    Refactor API index.py to center-align the welcome message on the home page

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.get(path="/")
 def home() -> HTMLResponse:
     return HTMLResponse(
-        content="<h1>Welcome to the Backend of <a href='https://MH0386.github.io'>MH0386.github.io</a></h1>",
+        content="<h1 style='text-align: center;'>Welcome to the Backend of <a href='https://MH0386.github.io'>MH0386.github.io</a></h1>",
         status_code=200,
     )
 
